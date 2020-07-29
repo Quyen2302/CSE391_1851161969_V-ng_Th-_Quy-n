a5d70209bdce62d754f9e9e2211a284496fd874f
Quyen2302 <67349663+Quyen2302@users.noreply.github.com>
2020-07-29 19:48:42 +0700

    reset homework4

--- HOMEWORK_04/bai1.01/js/script.js
+++ HOMEWORK_04/bai1.01/js/script.js
@@ -1,6 +1,11 @@
-
-$(document).ready(function(){
-	$(".circle").hover(function(){
-		$(this).hide();
-	});
-});
+var jFruit     = document.getElementById('txtFruit');
+var jShowFruit = document.getElementById('btnShowFruit');
+var jImgFruit  = document.getElementById('imgFruit');
+alert('Co chay den day') 
+//bat su kien
+jShowFruit.addEventListener('click',showResult);
+//khai bao chi tiet ham xu li du lieu
+function showResult(){
+    var getFruit = jFruit.value;
+    jImgFruit.setAttribute('src','images/'+getFruit+'.jpg');
+}
